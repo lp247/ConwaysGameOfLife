a27a12730ed3122a1b814adb84503fefc8c19362
Peter Lehnhardt <e0017@outlook.com>
2023-12-21 10:14:33 +0100

    Test and refactor setup.ts

--- src/setup.ts
+++ src/setup.ts
@@ -1,14 +1,15 @@
-import type {GridSize} from "./utils.js";
+import {type Grid, type GridSize} from "./utils.js";
 
-const RANDOM_THRESHOLD = 0.2;
+const PERCENTAGE_ALIVE = 0.5;
 
 const getGreatestCommonDivisor = (a: number, b: number): number => (b === 0 ? a : getGreatestCommonDivisor(b, a % b));
 
-const determineGridSize = (): GridSize => {
-    const clientHeight = document.documentElement.clientHeight;
-    const clientWidth = document.documentElement.clientWidth;
-    const gcd = getGreatestCommonDivisor(clientWidth, clientHeight);
-    return {width: clientWidth / gcd, height: clientHeight / gcd};
+const determineGridSize = (screenWidth: number, screenHeight: number): GridSize => {
+    if (screenWidth === 0 || screenHeight === 0) {
+        throw new Error("Screen width and height must be greater than 0!");
+    }
+    const gcd = getGreatestCommonDivisor(screenWidth, screenHeight);
+    return {width: screenWidth / gcd, height: screenHeight / gcd};
 };
 
 const generateDomCellString = (alive: boolean): string => {
@@ -16,18 +17,159 @@ const generateDomCellString = (alive: boolean): string => {
         return `<div class="cell alive"></div>`;
     }
     return `<div class="cell"></div>`;
-}
-
-const generateDomGrid = (element: HTMLElement, size: GridSize): void => {
-    element.style.setProperty("display", "grid");
-    element.style.setProperty("grid-template-columns", `repeat(${size.width}, 1fr)`);
-    element.style.setProperty("grid-template-rows", `repeat(${size.height}, 1fr)`);
-    element.innerHTML = Array(size.width * size.height).fill("").map(() => generateDomCellString(Math.random() > RANDOM_THRESHOLD)).join("");
 };
+
+const generateDomGridString = (grid: Grid): string => {
+    return grid.map((row) => row.map((cell) => generateDomCellString(cell)).join("")).join("");
+};
+
+const generateInitialGrid = (size: GridSize, fill: (x: number, y: number) => boolean): Grid => {
+    return (
+        Array(size.height)
+            .fill(Array(size.width).fill(false))
+            // @ts-ignore
+            .map((row, y) => row.map((_, x) => fill(x, y)))
+    );
+};
+
+const randomLifeGenerator = (percentageAlive: number) => (): boolean => Math.random() < percentageAlive;
 
 export const setupGrid = (container: HTMLElement) => {
-    const gridSize = determineGridSize();
+    const {clientWidth, clientHeight} = document.documentElement;
+    const gridSize = determineGridSize(clientWidth, clientHeight);
     console.log(`Grid size: ${gridSize.width}x${gridSize.height}`);
-    generateDomGrid(container, gridSize);
+    container.style.setProperty("display", "grid");
+    container.style.setProperty("grid-template-columns", `repeat(${gridSize.width}, 1fr)`);
+    container.style.setProperty("grid-template-rows", `repeat(${gridSize.height}, 1fr)`);
+    container.innerHTML = generateDomGridString(generateInitialGrid(gridSize, randomLifeGenerator(PERCENTAGE_ALIVE)));
     return gridSize;
 };
+
+if (import.meta.vitest) {
+    describe("getGreatestCommonDivisor", () => {
+        it("should return the correct GCD", () => {
+            expect(getGreatestCommonDivisor(10, 5)).toBe(5);
+            expect(getGreatestCommonDivisor(10, 3)).toBe(1);
+            expect(getGreatestCommonDivisor(10, 0)).toBe(10);
+            expect(getGreatestCommonDivisor(0, 10)).toBe(10);
+            expect(getGreatestCommonDivisor(0, 0)).toBe(0);
+            expect(getGreatestCommonDivisor(32, 24)).toBe(8);
+        });
+    });
+    describe("determineGridSize", () => {
+        it("should return the correct grid size", () => {
+            expect(determineGridSize(10, 10)).toStrictEqual({width: 1, height: 1});
+            expect(determineGridSize(10, 5)).toStrictEqual({width: 2, height: 1});
+            expect(determineGridSize(5, 10)).toStrictEqual({width: 1, height: 2});
+            expect(determineGridSize(32, 24)).toStrictEqual({width: 4, height: 3});
+        });
+        it("should throw an error if one dimension is 0", () => {
+            expect(() => determineGridSize(0, 0)).toThrow();
+            expect(() => determineGridSize(10, 0)).toThrow();
+            expect(() => determineGridSize(0, 10)).toThrow();
+        });
+    });
+    describe("generateDomCellString", () => {
+        it("should generate alive cells correctly", () => {
+            expect(generateDomCellString(true)).toBe(`<div class="cell alive"></div>`);
+        });
+        it("should generate dead cells correctly", () => {
+            expect(generateDomCellString(false)).toBe(`<div class="cell"></div>`);
+        });
+    });
+    describe("generateDomGridString", () => {
+        it("should generate a grid of the correct size", () => {
+            expect(
+                generateDomGridString([
+                    [false, false, false],
+                    [true, false, false],
+                    [false, false, false],
+                ]),
+            ).toBe(
+                '<div class="cell"></div>' +
+                    '<div class="cell"></div>' +
+                    '<div class="cell"></div>' +
+                    '<div class="cell alive"></div>' +
+                    '<div class="cell"></div>' +
+                    '<div class="cell"></div>' +
+                    '<div class="cell"></div>' +
+                    '<div class="cell"></div>' +
+                    '<div class="cell"></div>',
+            );
+            expect(
+                generateDomGridString([
+                    [false, false, false],
+                    [false, false, false],
+                    [false, false, false]
+                ])
+            ).toBe(
+                '<div class="cell"></div>' +
+                    '<div class="cell"></div>' +
+                    '<div class="cell"></div>' +
+                    '<div class="cell"></div>' +
+                    '<div class="cell"></div>' +
+                    '<div class="cell"></div>' +
+                    '<div class="cell"></div>' +
+                    '<div class="cell"></div>' +
+                    '<div class="cell"></div>',
+            );
+            expect(
+                generateDomGridString([
+                    [true, true, true],
+                    [true, true, true],
+                    [true, true, true]
+                ])
+            ).toBe(
+                '<div class="cell alive"></div>' +
+                    '<div class="cell alive"></div>' +
+                    '<div class="cell alive"></div>' +
+                    '<div class="cell alive"></div>' +
+                    '<div class="cell alive"></div>' +
+                    '<div class="cell alive"></div>' +
+                    '<div class="cell alive"></div>' +
+                    '<div class="cell alive"></div>' +
+                    '<div class="cell alive"></div>',
+            );
+        });
+    });
+    describe("generateInitialGrid", () => {
+        it("should generate a grid of the correct size", () => {
+            expect(generateInitialGrid({width: 3, height: 3}, () => false)).toStrictEqual([
+                [false, false, false],
+                [false, false, false],
+                [false, false, false],
+            ]);
+            expect(generateInitialGrid({width: 3, height: 3}, () => true)).toStrictEqual([
+                [true, true, true],
+                [true, true, true],
+                [true, true, true],
+            ]);
+            expect(generateInitialGrid({width: 3, height: 3}, (x, y) => x === y)).toStrictEqual([
+                [true, false, false],
+                [false, true, false],
+                [false, false, true],
+            ]);
+        });
+    });
+    describe("randomLifeGenerator", () => {
+        beforeAll(() => {
+            vi.spyOn(Math, "random").mockReturnValue(0.5);
+        });
+
+        afterAll(() => {
+            vi.restoreAllMocks();
+        });
+
+        it("should return true if random number is smaller than percentageAlive", () => {
+            expect(randomLifeGenerator(0.6)()).toBe(true);
+        });
+
+        it("should return false if random number is greater than percentageAlive", () => {
+            expect(randomLifeGenerator(0.4)()).toBe(false);
+        });
+
+        it("should return false if random number is equal to percentageAlive", () => {
+            expect(randomLifeGenerator(0.5)()).toBe(false);
+        });
+    });
+}
